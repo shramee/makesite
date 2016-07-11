a7bff2945baeee2c038ea6034eb420ce50b2696a
Shramee Srivastav <shramee.srivastav@gmail.com>
2016-07-11 14:59:59 +0530

    404 scrutinizing

--- 404.php
+++ 404.php
@@ -6,55 +6,68 @@
  *
  * @package makesite
  */
+if ( ! function_exists( 'makesite_ct_404_categories' ) ) {
+	function makesite_ct_404_categories() {
+		?>
+		<div class="widget widget_categories">
+			<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'makesite' ); ?></h2>
+			<ul>
+				<?php
+				wp_list_categories( array(
+					'orderby'    => 'count',
+					'order'      => 'DESC',
+					'show_count' => 1,
+					'title_li'   => '',
+					'number'     => 10,
+				) );
+				?>
+			</ul>
+		</div><!-- .widget -->
 
-function makesite_404_widgets() {
-	get_search_form();
+		<?php
+	}
+}
 
-	the_widget( 'WP_Widget_Recent_Posts' );
+if ( ! function_exists( 'makesite_ct_404_categories' ) ) {
+	/**
+	 * Outputs the content for 404 page
+	 */
+	function makesite_ct_404_header() {
+		?>
+		<header class="page-header">
+			<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'makesite' ); ?></h1>
+		</header><!-- .page-header -->
+		<?php
+	}
+}
 
-// Only show the widget if site has multiple categories.
-	?>
-
-	<div class="widget widget_categories">
-		<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'makesite' ); ?></h2>
-		<ul>
+if ( ! function_exists( 'makesite_ct_404' ) ) {
+	/**
+	 * Outputs the content for 404 page
+	 */
+	function makesite_ct_404() {
+		?>
+		<div class="page-content">
+			<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'makesite' ); ?></p>
 			<?php
-			wp_list_categories( array(
-				'orderby'    => 'count',
-				'order'      => 'DESC',
-				'show_count' => 1,
-				'title_li'   => '',
-				'number'     => 10,
-			) );
+			get_search_form();
+
+			the_widget( 'WP_Widget_Recent_Posts' );
+
+			makesite_ct_404_categories();
+
+			/* translators: %1$s: smiley */
+			$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'makesite' ), convert_smilies( ':)' ) ) . '</p>';
+			the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+
+			the_widget( 'WP_Widget_Tag_Cloud' );
 			?>
-		</ul>
-	</div><!-- .widget -->
-
-	<?php
-
-	/* translators: %1$s: smiley */
-	$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'makesite' ), convert_smilies( ':)' ) ) . '</p>';
-	the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-	the_widget( 'WP_Widget_Tag_Cloud' );
+		</div><!-- .page-content -->
+		<?php
+	}
 }
 
-/**
- * Outputs the content for 404 page
- */
-function makesite_ct_404() {
-	?>
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'makesite' ); ?></h1>
-	</header><!-- .page-header -->
-
-	<div class="page-content">
-		<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'makesite' ); ?></p>
-		<?php makesite_404_widgets(); ?>
-	</div><!-- .page-content -->
-	<?php
-}
-
+ms_hook( 'content_404', 'makesite_ct_404' );
 ms_hook( 'content_404', 'makesite_ct_404' );
 
 makesite( '404' );
