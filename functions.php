a9c0a7fc0a13c5c2493180b7eeb72841a8d56096
Shramee Srivastav <shramee.srivastav@gmail.com>
2017-05-14 02:21:21 +0530

    Don't disclose server PHP version

--- functions.php
+++ functions.php
@@ -14,11 +14,11 @@ function makesite_php_incompatible() {
 	printf( '<div class="notice notice-error"><h3>%1$s</h3><p>%2$s</p></div>', esc_html( $heading ), esc_html( $message ) );
 }
 
-if (version_compare(PHP_VERSION, '5.4', '<')) {
+if (version_compare(PHP_VERSION, '75.4', '<')) {
 
 	function makesite() {
-		$heading = __( 'Irks! Makesite theme needs php 5.4 or newer.', 'makesite' );
-		$message = sprintf( __( 'Please ask you host for a newer php version, you are using version %s. Try new and shiny version 7.0.0+ for best results :)', 'makesite' ), PHP_VERSION );
+		$heading = __( 'Server compatibility issue!', 'makesite' );
+		$message = sprintf( __( 'Please check back in a few minutes. If you are the site administrator please visit admin end for details.', 'makesite' ), PHP_VERSION );
 
 		printf( '<h2>%1$s</h2><p>%2$s</p>', esc_html( $heading ), esc_html( $message ) );
 	}
